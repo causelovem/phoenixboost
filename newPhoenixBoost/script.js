0eb8113c734ad2e0814d62f5705d14a78ddcb815
Kozlov Michael <rat.taurus@gmail.com>
2018-01-02 21:15:09 +0300

    last form

--- newPhoenixBoost/script.js
+++ newPhoenixBoost/script.js
@@ -15,16 +15,25 @@ function goToBlockByID(id)
 function processOrder(id)
 {
     if (id == "buttonRank")
+    {
         countRankPrice();
+        document.getElementById('invisibleBoostType').value = "rank boost";
+    }
     else
     if (id == "buttonPlacement")
+    {
         countPlacementsPrice();
+        document.getElementById('invisibleBoostType').value = "placements";
+    }
     else
     if (id == "buttonDecay")
+    {
         countAntiDecayPrice();
+        document.getElementById('invisibleBoostType').value = "anti-decay";
+    }
 
     $('.transparentLayer').css("display", "block");
-    $('.cw1').css("display", "block");
+    $('.orderBoard').css("display", "block");
 }
 /*******/
 
@@ -35,16 +44,20 @@ function checkEmail()
     if ($('#inpEmail').val() == '')
     {
         $('#inpEmail').css("box-shadow", "0 5px 0 0 #C33C3C");
+        return 1;
     }
     else
     if ($('#inpEmail').val().search(pattern) != 0)
     {
         $('#inpEmail').css("box-shadow", "0 5px 0 0 #C33C3C");
+        return 1;
     }
     else
     {
         $('#inpEmail').css("box-shadow", "0 5px 0 0 #26C281");
     }
+
+    return 0;
 }
 
 function checkFullName()
@@ -52,12 +65,14 @@ function checkFullName()
     if ($('#inpName').val() == '')
     {
         $('#inpName').css("box-shadow", "0 5px 0 0 #C33C3C");
+        return 1;
     }
     else
     {
         $('#inpName').css("box-shadow", "0 5px 0 0 #26C281");
     }
 
+    return 0;
 }
 /*******/
 
@@ -462,9 +477,10 @@ document.getElementById('inpEmail').value = "";
 document.getElementById('inpName').value = "";
 document.getElementById('inpMessage').value = "";
 
+document.getElementById('invisibleBoostType').value = "rank boost";
+
 // document.getElementById('faq').scrollTop = 0;
 // document.getElementById('contacts').scrollTop = 0;
-
 
 
 $(document).ready(function(){ $('#leftUp').click(function() { displayLeftPic(Number(curSR) + 50); });});
@@ -514,10 +530,10 @@ $(document).ready(function()
 
 $(document).ready(function()
 {
-    $('.nb4').click(function()
+    $('.faqButton').click(function()
     {
         $('.transparentLayer').css("display", "block");
-        $('.cw3').css("display", "block");
+        $('.faq').css("display", "block");
     });
 });
 
@@ -526,9 +542,9 @@ $(document).ready(function()
     $('.closeButton').click(function()
     {
         $('.transparentLayer').css("display", "none");
-        $('.cw1').css("display", "none");
-        $('.cw2').css("display", "none");
-        $('.cw3').css("display", "none");
+        $('.orderBoard').css("display", "none");
+        $('.submitBoard').css("display", "none");
+        $('.faq').css("display", "none");
     });
 });
 
@@ -537,9 +553,88 @@ $(document).ready(function()
     $('.transparentLayer').click(function()
     {
         $('.transparentLayer').css("display", "none");
-        $('.cw1').css("display", "none");
-        $('.cw2').css("display", "none");
-        $('.cw3').css("display", "none");
+        $('.orderBoard').css("display", "none");
+        $('.submitBoard').css("display", "none");
+        $('.faq').css("display", "none");
+    });
+});
+
+$(document).ready(function()
+{
+    $('.backButton').click(function()
+    {
+        $('.orderBoard').css("display", "block");
+        $('.submitBoard').css("display", "none");
+    });
+});
+
+$(document).ready(function()
+{
+    $('#purchaseButton').click(function()
+    {
+        if ((checkEmail()) || (checkFullName()))
+            return;
+
+        if (document.getElementById('invisibleBoostType').value == "rank boost")
+        {
+            if (currency == "usd")
+            document.getElementById('submitOrderDetails').innerHTML = "Your boost from "
+             + Number(curSR) + " to " + Number(disSR) + " costs "
+               + boostPrice.toFixed(0) + "$";
+            else
+            if (currency == "eur")
+                document.getElementById('submitOrderDetails').innerHTML = "Your boost from "
+                 + Number(curSR) + " to " + Number(disSR) + " costs "
+                   + boostPrice.toFixed(0) + "&euro;";
+            else
+            if (currency == "rub")
+                document.getElementById('submitOrderDetails').innerHTML = "Your boost from "
+                 + Number(curSR) + " to " + Number(disSR) + " costs "
+                   + boostPrice.toFixed(0) + "&#8381;";
+        }
+        else
+        if (document.getElementById('invisibleBoostType').value == "placements")
+        {
+            if (currency == "usd")
+                document.getElementById('submitOrderDetails').innerHTML = "Your placements to "
+                 + document.getElementById('invisiblePlacementsRank').value + " costs "
+                   + placementsPrice.toFixed(0) + "$";
+            else
+            if (currency == "eur")
+                document.getElementById('submitOrderDetails').innerHTML = "Your placements to "
+                 + document.getElementById('invisiblePlacementsRank').value + " costs "
+                   + placementsPrice.toFixed(0) + "&euro;";
+            else
+            if (currency == "rub")
+                document.getElementById('submitOrderDetails').innerHTML = "Your placements to "
+                 + document.getElementById('invisiblePlacementsRank').value + " costs "
+                   + placementsPrice.toFixed(0) + "&#8381;";
+        }
+        else
+        if (document.getElementById('invisibleBoostType').value == "anti-decay")
+        {
+            if (currency == "usd")
+            document.getElementById('submitOrderDetails').innerHTML = "Your anti-decay at the rank of "
+             + document.getElementById('invisibleAntiDecayRank').value + " costs "
+               + antiDecayPrice.toFixed(0) + "$";
+            else
+            if (currency == "eur")
+                document.getElementById('submitOrderDetails').innerHTML = "Your anti-decay at the rank of "
+                 + document.getElementById('invisibleAntiDecayRank').value + " costs "
+                   + antiDecayPrice.toFixed(0) + "&euro;";
+            else
+            if (currency == "rub")
+                document.getElementById('submitOrderDetails').innerHTML = "Your anti-decay at the rank of "
+                 + document.getElementById('invisibleAntiDecayRank').value + " costs "
+                   + antiDecayPrice.toFixed(0) + "&#8381;";
+        }
+
+        document.getElementById('submitEmail').innerHTML = $('#inpEmail').val();
+        document.getElementById('submitName').innerHTML = $('#inpName').val();
+        document.getElementById('submitMessage').innerHTML = $('#inpMessage').val();
+
+        $('.orderBoard').css("display", "none");
+        $('.submitBoard').css("display", "block");
     });
 });
 
